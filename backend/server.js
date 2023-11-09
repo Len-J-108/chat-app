66dcc394481fed264f3fd053c6d2c9bb200cf0ef
Lennart Jansen <lennart.official.jansen@gmail.com>
2023-11-09 16:37:17 +0100

    socket first move

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 
 dotenv.config(); // Initialize dotenv
 const { PORT } = process.env; // environment variables
 
 const app = express();
 app.use(express.json()); // Body Parser
+const server = createServer(app);
+const io = new Server(server);
 
 //------------------------------------------------------------------------------------
 // test example from socketIO homepage
@@ -15,6 +19,10 @@ app.get('/', (req, res) => {
 
 //------------------------------------------------------------------------------------
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(PORT, () => {
   console.log(`Server is listening to port: ${PORT}`);
 });
